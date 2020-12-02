64c599995af24599ea5d3c086ea2bb0e3c99392a
H4ckint0sh <alijan.adeli@icloud.com>
2020-12-02 21:31:41 +0100

    added searchbar

--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import { Title, useTheme } from "react-native-paper";
+import Search from "../components/Searchbar";
 
 const SearchScreen = () => {
   const theme = useTheme();
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
     },
+    search: {},
     text: {
       color: theme.colors.primary,
     },
   });
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <Search style={styles.search} />
       <Title style={styles.text}>Search Screen</Title>
-    </View>
+    </SafeAreaView>
   );
 };
 
